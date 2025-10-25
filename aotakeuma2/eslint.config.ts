0836b617b4b78194c38372c77934c2ef9e3e9653
Bmbo-sprg <86242629+Bmbo-sprg@users.noreply.github.com>
2025-10-26 03:11:40 +0900

    eslint

---
+++ aotakeuma2/eslint.config.ts
@@ -0,0 +1,60 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import prettier from "eslint-config-prettier/flat";
+import pluginImport from "eslint-plugin-import";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  tseslint.configs.recommended,
+  pluginImport.flatConfigs.recommended,
+  pluginImport.flatConfigs.typescript,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  prettier,
+
+  {
+    ignores: [
+      "build/**",
+      ".react-router/**",
+      ".wrangler/**",
+      "node_modules/**",
+      "public/**",
+    ],
+  },
+
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
+
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        typescript: true,
+      },
+    },
+  },
+
+  {
+    files: ["**/*.{ts,tsx}"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+]);
