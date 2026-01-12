e2afc48c92f57adcd39c046b3273849b310dab2f
Bmbo-sprg <86242629+Bmbo-sprg@users.noreply.github.com>
2026-01-12 19:38:55 +0900

    fix eslint

--- aotakeuma2/eslint.config.ts
+++ aotakeuma2/eslint.config.ts
@@ -1,13 +1,11 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import { configs as tseslintConfigs } from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import prettier from "eslint-config-prettier/flat";
 import pluginImport from "eslint-plugin-import";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  tseslint.configs.recommended,
+  tseslintConfigs.recommended,
   pluginImport.flatConfigs.recommended,
   pluginImport.flatConfigs.typescript,
   pluginReact.configs.flat.recommended,
@@ -21,16 +19,8 @@ export default defineConfig([
       ".wrangler/**",
       "node_modules/**",
       "public/**",
+      "worker-configuration.d.ts",
     ],
-  },
-
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
-    },
   },
 
   {
