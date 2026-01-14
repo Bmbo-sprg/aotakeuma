00ae5587ac2b86a73d50739a08db2e7fb05d3fc9
Bmbo-sprg <86242629+Bmbo-sprg@users.noreply.github.com>
2026-01-15 07:31:10 +0900

    temp

--- aotakeuma2/app/root.tsx
+++ aotakeuma2/app/root.tsx
@@ -23,6 +23,7 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+// TODO: add global meta (ogp etc.)
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -45,6 +46,7 @@ export default function App() {
   return <Outlet />;
 }
 
+// TODO: ちゃんとしたエラーページを作る
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   let message = "Oops!";
   let details = "An unexpected error occurred.";
--- aotakeuma2/app/routes.ts
+++ aotakeuma2/app/routes.ts
@@ -1,3 +1,31 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  // 自己紹介、works 一覧 + see more リンク、events 一覧 + see more リンク、contact 概要 + see more リンク、posts (Twitter, note?)
+  index("routes/home.tsx"),
+
+  // route("works", "routes/works/layout.tsx", [
+  //   // 検索バー、work type でフィルタリング、ソートなど
+  //   index("routes/works/index.tsx"), // /works
+  //   route("albums/:workId", "routes/works/album.tsx"), // /works/albums/:workId
+  //   route("musics/:workId", "routes/works/music.tsx"), // /works/musics/:workId
+  //   route("videos/:workId", "routes/works/video.tsx"), // /works/videos/:workId
+  //   route("games/:workId", "routes/works/game.tsx"), // /works/games/:workId
+  //   route("others/:workId", "routes/works/otherWork.tsx"), // /works/others/:workId
+  // ]),
+
+  // route("events", "routes/events/layout.tsx", [
+  //   // 検索バー、event type でフィルタリング、ソートなど
+  //   index("routes/events/index.tsx"), // /events
+  //   route("performances/:eventId", "routes/events/performance.tsx"), // /events/performances/:eventId
+  //   route("exhibitions/:eventId", "routes/events/exhibition.tsx"), // /events/exhibitions/:eventId
+  //   route("others/:eventId", "routes/events/otherEvent.tsx"), // /events/others/:eventId
+  // ]),
+
+  // route("contact", "routes/contact.tsx"), // /contact
+
+  // route("aonote", "routes/aliases/aonote.tsx"), // /aonote
+  // route("digimoca", "routes/aliases/digimoca.tsx"), // /digimoca
+  // route("haruame-ost", "routes/aliases/haruame-ost.tsx"), // /haruame-ost
+  // // TODO: add more...
+] satisfies RouteConfig;
