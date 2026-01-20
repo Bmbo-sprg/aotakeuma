c1e31485cff18c6a4efc81cbcafecaa8cb8dba01
Bmbo-sprg <86242629+Bmbo-sprg@users.noreply.github.com>
2026-01-21 03:59:12 +0900

    delete video from works

--- aotakeuma2/app/contents/works/index.ts
+++ aotakeuma2/app/contents/works/index.ts
@@ -1,8 +1,7 @@
 import type { Work } from "~/types";
 import { albums } from "./albums";
 import { musics } from "./musics";
-import { videos } from "./videos";
 import { games } from "./games";
 
 // how to add other works: just create under app/contents/works/ and import here
-export const works: Work[] = [...albums, ...musics, ...videos, ...games];
+export const works: Work[] = [...albums, ...musics, ...games];
