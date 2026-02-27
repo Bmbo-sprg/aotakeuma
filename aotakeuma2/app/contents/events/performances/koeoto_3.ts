2265ef5e9f9e5bd741efedc5aa89c9efa0e48fe2
Bmbo-sprg <86242629+Bmbo-sprg@users.noreply.github.com>
2026-02-28 05:34:13 +0900

    add koeoto_3

---
+++ aotakeuma2/app/contents/events/performances/koeoto_3.ts
@@ -0,0 +1,29 @@
+import type { Performance } from "~/types";
+
+export const koeoto_3: Performance = {
+  type: "performance",
+  id: "koeoto_3",
+  title: "声音文脈 Vol.3",
+  description: `
+レギュラーDJとしての初出演！
+Show must go on、そして、I must go (on) の気持ちで群像劇をやらせていただきました
+唯世かのんって良くて〜って話
+`,
+  tags: [],
+  date: new Date("2026-02-22"),
+  location: "IV AKIHABARA",
+  links: [
+    {
+      platform: "twinvite",
+      url: "https://twvt.me/KoeotoBunmyaku_2602",
+    },
+    {
+      platform: "twitter",
+      url: "https://x.com/aotakeuma/status/2025554582462660987",
+    },
+    {
+      platform: "mixcloud",
+      url: "https://www.mixcloud.com/%E7%AB%B9%E9%A6%AC%E3%81%82%E3%81%8A/%E5%A3%B0%E9%9F%B3%E6%96%87%E8%84%88-vol3-%E7%AB%B9%E9%A6%AC%E3%81%82%E3%81%8A-%E5%86%8D%E7%8F%BEmix",
+    },
+  ],
+};
--- aotakeuma2/app/contents/events/performances/kuvocalodj_2.ts
+++ aotakeuma2/app/contents/events/performances/kuvocalodj_2.ts
@@ -16,5 +16,9 @@ export const kuvocalodj_2: Performance = {
       platform: "twitter",
       url: "https://x.com/aotakeuma/status/1826231469817401758",
     },
+    {
+      platform: "mixcloud",
+      url: "https://www.mixcloud.com/%E7%AB%B9%E9%A6%AC%E3%81%82%E3%81%8A/%E4%BA%AC%E5%A4%A7%E3%83%9C%E3%82%AB%E3%83%ADdj-vol2-%E7%AB%B9%E9%A6%AC%E3%81%82%E3%81%8A-mix",
+    },
   ],
 };
