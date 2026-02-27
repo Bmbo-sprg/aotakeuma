d5408faee3e463fb9a10b14eff3cab6e5a916c70
Bmbo-sprg <86242629+Bmbo-sprg@users.noreply.github.com>
2026-02-28 06:38:42 +0900

    add MixcloudIframe

---
+++ aotakeuma2/app/components/MixcloudIframe/MixcloudIframe.tsx
@@ -0,0 +1,40 @@
+type MixcloudIframeProps = {
+  url: string;
+  width?: number | string;
+  height?: number | string;
+};
+
+const urlToEmbed = (url: string) => {
+  const match = url.match(
+    /https?:\/\/www\.mixcloud\.com\/([^\/]+)\/([^\/]+)\/?/
+  );
+  if (!match) {
+    return null;
+  }
+  const [, user, id] = match;
+  return `https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2F${user}%2F${id}%2F`;
+};
+
+export const MixcloudIframe = ({ url, width, height }: MixcloudIframeProps) => {
+  const embedUrl = urlToEmbed(url);
+  if (!embedUrl) {
+    return null;
+  }
+
+  return (
+    <iframe
+      style={{
+        borderRadius: "12px",
+        borderWidth: "2px",
+        borderColor: "#ccc",
+        boxSizing: "content-box",
+      }}
+      src={embedUrl}
+      width={width || "100%"}
+      height={height || "120"}
+      allowFullScreen
+      allow="encrypted-media; fullscreen; autoplay; idle-detection; speaker-selection; web-share;"
+      referrerPolicy="strict-origin-when-cross-origin"
+    />
+  );
+};
