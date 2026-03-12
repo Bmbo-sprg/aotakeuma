7b5903df4b69bcb1d5f1dd16e18ba8d4f7559adf
Bmbo-sprg <86242629+Bmbo-sprg@users.noreply.github.com>
2026-03-12 22:29:58 +0900

    update dddd_1

--- aotakeuma2/app/contents/events/performances/dddd_1.ts
+++ aotakeuma2/app/contents/events/performances/dddd_1.ts
@@ -6,8 +6,8 @@ export const dddd_1: Performance = {
   title: "秘密結社D.D.D.D. 20260311",
   description: `
 とある秘密結社に集った構成員たちによるDJしてみた系オールジャンルDJイベント、開幕―――
-竹馬あおは、「ボカロ／合成音声と、ボカロ／合成音声作曲家によるオタクソング」を中心にやっていきます。
-（予定）
+2番手で、「ボカロ／合成音声作曲家による人間ボーカル曲」縛りでDJしました。
+初めてアイマス・ラブライブ中心の選曲でやりたい放題
 `,
   tags: [],
   date: new Date("2026-03-11"),
@@ -15,5 +15,13 @@ export const dddd_1: Performance = {
   detailLink: {
     platform: "twipla",
     url: "https://twipla.jp/events/718434",
+  },
+  twitterLink: {
+    platform: "twitter",
+    url: "https://x.com/aotakeuma/status/2031739007919587345",
+  },
+  mixcloudLink: {
+    platform: "mixcloud",
+    url: "https://www.mixcloud.com/%E7%AB%B9%E9%A6%AC%E3%81%82%E3%81%8A/%E7%A7%98%E5%AF%86%E7%B5%90%E7%A4%BEdddd-20260311-%E5%86%8D%E7%8F%BEmix",
   },
 };
