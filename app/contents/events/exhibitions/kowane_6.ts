51cf67f5111168862f2c3065439c853be08c4b5a
Bmbo-sprg <86242629+Bmbo-sprg@users.noreply.github.com>
2026-05-22 19:39:17 +0900

    update kowane_6

--- app/contents/events/exhibitions/kowane_6.ts
+++ app/contents/events/exhibitions/kowane_6.ts
@@ -1,5 +1,4 @@
 import { aonote } from "../../works/albums/aonote";
-import { syngularity } from "../../works/albums/syngularity";
 import { yohkoh } from "../../works/albums/yohkoh";
 import { esora_key } from "../../works/esora_key";
 import type { Exhibition } from "~/types";
@@ -8,10 +7,10 @@ export const kowane_6: Exhibition = {
   type: "exhibition",
   id: "kowane_6",
   title: "声音の宴 6次会",
-  description: "めちゃめちゃ元気だったら新譜を持っていきたい！！！！！！！",
+  description: "充電中...",
   tags: [],
   date: new Date("2026-05-24"),
   location: "大田区産業プラザ PiO F-01",
   links: [],
-  catalog: [yohkoh, syngularity, aonote, esora_key],
+  catalog: [yohkoh, aonote, esora_key],
 };
